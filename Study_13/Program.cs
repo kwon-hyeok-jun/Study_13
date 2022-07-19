d0735d77f8f817a22171489ae65ad2764b919325
Kwon <kkynks0@naver.com>
2022-07-19 19:48:18 +0900

    초기 커밋.

---
+++ Study_13/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+/*
+class SequenceDemo
+{
+    static void Main(string[] args)
+    {
+        int kor = 100;
+        int eng = 90;
+
+        int tot = 0;
+        double avg = 0.0;
+
+        tot = kor + eng;
+        avg = tot / 2.0;
+
+        Console.WriteLine("총점 : {0}", tot);
+        Console.WriteLine("평균 : {0:F1}", avg);
+    }
+}
+*/
+
+
+/*
+class IfDemo
+{
+    static void Main(string[] args)
+    {
+        int x = 40;
+
+        if (x == 20)
+        {
+            Console.WriteLine($"x는 {x}입니다.");
+        }
+        else if (x == 30)
+        {
+            Console.WriteLine($"x는 {x}입니다.");
+        }
+        else
+        {
+            Console.WriteLine($"다 틀렸습니다.");
+        }
+    }
+}
+*/
+
+
+
